4d173b989320b24eef844b78b75ab15887452b12
Jabin <jabin@mister-king.co.uk>
2021-10-16 00:28:11 +0100

    Fix categories not showing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -54,6 +54,7 @@ export default {
     this.$store.dispatch("refreshToken");
     this.$store.dispatch("requestUserData");
     this.$store.dispatch("requestCurrentGroup");
+    this.$store.dispatch("requestCategories");
     this.$store.dispatch("requestTags");
     this.$store.dispatch("requestAppInfo");
     this.$store.dispatch("requestSiteSettings");
