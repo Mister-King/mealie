e45510c211c6b150487912a54e8909e1ee1e84c6
Jabin <jabin@mister-king.co.uk>
2021-10-15 23:45:29 +0100

    Fix sort by ratings

--- frontend/src/utils/recipe.js
+++ frontend/src/utils/recipe.js
@@ -18,7 +18,7 @@ export const recipe = {
     list.sort((a, b) => (a.dateUpdated > b.dateUpdated ? -1 : 1));
   },
   sortByRating(list) {
-    list.sort((a, b) => (a.rating > b.rating ? -1 : 1));
+    list.sort((a, b) => (this.getAverageRating(JSON.parse(a.ratings)) > this.getAverageRating(JSON.parse(b.ratings)) ? -1 : 1));
   },
   /**
    *
